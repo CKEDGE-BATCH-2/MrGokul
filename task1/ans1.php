977b62934b9630071c8ebd8fc40fa6e6bcaec953
MrGokul27 <137751758+MrGokul27@users.noreply.github.com>
2023-07-04 10:39:00 +0530

    PHP task1
    i have uploaded my php task1 files

---
+++ task1/ans1.php
@@ -0,0 +1,4 @@
+<?php
+$letters = array(0 => 'aaa',1 => 'bbb',2 => 'ccc',3 => 'ddd',4 => 'eee');
+echo count($letters);
+?>
---
+++ task1/ans2.php
@@ -0,0 +1,6 @@
+<?php
+$minmax = array(1, 2, 3, 4, 5);
+echo(min($minmax));
+echo("\n");
+echo(max($minmax));
+?>
